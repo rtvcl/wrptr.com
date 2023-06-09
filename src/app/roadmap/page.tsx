28dedb5da7a4a1906e0de164b78cd96bdd3bf964
rtcl <rtvcl.boss@gmail.com>
2023-06-09 19:50:51 +0700

    feat: refactor roadmap element

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,11 +1,6 @@
+import { RoadmapSection } from "@/components/roadmap";
 import React from "react";
-import { IconType } from "react-icons/lib";
-import {
-  TiInputChecked,
-  TiSocialTumbler,
-  TiSocialTumblerCircular,
-  TiTimes,
-} from "react-icons/ti";
+import { TiSocialTumbler } from "react-icons/ti";
 
 type Props = {};
 
@@ -14,23 +9,23 @@ const RoadmapPage = (props: Props) => {
     <section className="max-w-screen-lg mx-4 lg:mx-auto">
       <h1 className="mb-4 text-4xl font-black ">/roadmap</h1>
       <div className="space-y-4">
-        <p>
+        <p className="max-w-xl">
           Welcome to the roadmap page of my personal website! Here, I&apos;m
           excited to share with you the exciting plans I have in store for the
           future development of my website. Below, you&apos;ll find a glimpse
           into the exciting features and enhancements I have planned for my
           website.
         </p>
-        <RoadmapList
+        <RoadmapSection
           title="website"
           itemList={[
             {
               title: "General Information",
-              icon: TiSocialTumbler,
+              Icon: TiSocialTumbler,
               content: [
                 { roadmapTitle: "asdas", isDone: true },
                 { roadmapTitle: "adasjndijan", isDone: false },
-                { roadmapTitle: "adasd", isDone: false }
+                { roadmapTitle: "adasd", isDone: false },
               ],
             },
           ]}
@@ -39,52 +34,5 @@ const RoadmapPage = (props: Props) => {
     </section>
   );
 };
-
-type RoadmapGroup = {
-  title: string;
-  icon: IconType;
-  content: RoadmapItem[];
-};
-
-type RoadmapItem = {
-  isDone: boolean;
-  roadmapTitle: string;
-};
-
-const RoadmapList = ({
-  title,
-  itemList,
-}: {
-  title: string;
-  itemList: RoadmapGroup[];
-}) => (
-  <>
-    <h2 className="flex items-center text-2xl font-bold">
-      <span className="text-3xl">#</span>
-      {title}
-    </h2>
-    <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-      {itemList.map((item, index) => (
-        <li
-          key={item.title}
-          className="flex bg-white flex-col items-start space-y-4 retro-shadow border-[#000000] border-[2px] p-4"
-        >
-          <div className="retro-shadow p-2 border-[#000000] border-[2px] bg-lime-500 text-6xl text-white">
-            <item.icon />
-          </div>
-          <h4 className="text-lg font-black">{item.title}</h4>
-          <ul className="space-y-2">
-            {item.content.map((roadmapItem, index) => (
-              <li key={index} className="flex items-center space-x-2">
-                {roadmapItem.isDone ? <TiInputChecked /> : <TiTimes />}
-                <span>{roadmapItem.roadmapTitle}</span>
-              </li>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </ul>
-  </>
-);
 
 export default RoadmapPage;
---
+++ src/components/roadmap.tsx
@@ -0,0 +1,67 @@
+import { IconType } from "react-icons/lib";
+import { TiInputChecked, TiTimes } from "react-icons/ti";
+import React from "react";
+
+type RoadmapGroup = {
+  title: string;
+  Icon: IconType;
+  content: RoadmapItem[];
+};
+
+type RoadmapItem = {
+  isDone: boolean;
+  roadmapTitle: string;
+};
+
+export const RoadmapSection = ({
+  title,
+  itemList,
+}: {
+  title: string;
+  itemList: RoadmapGroup[];
+}) => (
+  <>
+    <h2 className="flex items-center text-2xl font-bold">
+      <span className="text-3xl">#</span>
+      {title}
+    </h2>
+    <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      {itemList.map((item, index) => (
+        <RoadmapCard
+          title={item.title}
+          Icon={item.Icon}
+          content={item.content}
+          key={index}
+        />
+      ))}
+    </ul>
+  </>
+);
+
+export const RoadmapCard = ({ title, Icon, content }: RoadmapGroup) => {
+  return (
+    <li
+      key={title}
+      className="flex bg-white flex-col items-start space-y-4 retro-shadow border-[#000000] border-[2px] p-4"
+    >
+      <div className="retro-shadow p-2 border-[#000000] border-[2px] bg-lime-500 text-6xl text-white">
+        {<Icon />}
+      </div>
+      <h4 className="text-lg font-black">{title}</h4>
+      <ul className="space-y-2">
+        {content.map((roadmapItem, index) => (
+          <RoadmapItem key={index} roadmapItem={roadmapItem} />
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+export const RoadmapItem = ({ roadmapItem }: { roadmapItem: RoadmapItem }) => {
+  return (
+    <li className="flex items-center space-x-2">
+      {roadmapItem.isDone ? <TiInputChecked /> : <TiTimes />}
+      <span>{roadmapItem.roadmapTitle}</span>
+    </li>
+  );
+};
