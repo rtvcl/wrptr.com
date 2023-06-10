32556bf22e636c1a03e714274e16242695872415
rtcl <rtvcl.boss@gmail.com>
2023-06-10 18:06:41 +0700

    feat: add contact-form functionality

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  let nodemailer = require("nodemailer");
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.GMAILUSER,
+      pass: process.env.GMAILPASS,
+    },
+    secure: true,
+  });
+
+  const bodyRequest = await req.json();
+
+  const mailData = {
+    from: 'local@local.com',
+    to: 'khainciller@gmail.com',
+    subject: `Message From ${bodyRequest.name}`,
+    text: bodyRequest.message,
+    html: `<div>${bodyRequest.message}</div>`
+   }
+
+   transporter.sendMail(mailData, function (err: any, info: any) {
+    if(err)
+      console.log(err)
+    else
+      console.log(info)
+  })
+
+  return NextResponse.json({ message: "success send message" });
+}
--- src/components/contact-me-form.tsx
+++ src/components/contact-me-form.tsx
@@ -1,26 +1,62 @@
+'use client'
+import { useState } from "react";
 import { BlackGrungeSection } from "./moodboard-section";
 
 type Props = {};
 
 const ContactMeForm = (props: Props) => {
-  return (
+    const [loading, setLoading] = useState(false);
+
+    async function handleSubmit(event: any) {
+		event.preventDefault();
+		setLoading(true);
+
+		const data = {
+			name: String(event.target.name.value),
+			email: String(event.target.email.value),
+			message: String(event.target.message.value),
+		};
+
+		const response = await fetch("/api/contact", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		});
+
+		if (response.ok) {
+			console.log("Message sent successfully");
+			setLoading(false);
+			// reset the form
+			event.target.name.value = "";
+			event.target.email.value = "";
+			event.target.message.value = "";
+		}
+		if (!response.ok) {
+			console.log("Error sending message");
+			setLoading(false);
+		}
+	}
+
+    return (
     <BlackGrungeSection className="max-w-screen-md px-8 py-8 mx-auto rounded-[28px] mb-4">
       <h2 className="font-bold text-2xl">Ready to connect?</h2>
       <p>
         Drop me a line using the contact form below. Let&apos;s chat about tech,
         web development, or anything that piques your interest.
       </p>
-      <form className="mt-6">
+      <form className="mt-6" onSubmit={handleSubmit}>
         <div className="mb-4">
           <label
-            htmlFor="full_name"
+            htmlFor="name"
             className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
           >
             Full name
           </label>
           <input
             type="text"
-            id="full_name"
+            id="name"
             placeholder="Luke Skywalker"
             className="w-full px-3 py-2 bg-white bg-opacity-10 placeholder-gray-300 border border-gray-300  focus:outline-none focus:ring focus:ring-[#2CE0AC] focus:border-[#2CE0AC]  placeholder-opacity-40"
             required
@@ -58,10 +94,11 @@ const ContactMeForm = (props: Props) => {
         </div>
         <div>
         <button
-            className=" px-6 whitespace-nowrap py-2 text-lg w-full font-extrabold bg-[#2CE0AC] retro-shadow hover:bg-[#A57EE5] transition-colors rotate-1 mt-4"
+        disabled={loading}
+            className=" disabled:bg-gray-400 px-6 whitespace-nowrap py-2 text-lg w-full font-extrabold bg-[#2CE0AC] retro-shadow hover:bg-[#A57EE5] transition-colors rotate-1 mt-4"
           >
             <div className="-rotate-1">
-            Send Message
+            {!loading ? 'Send Message': 'Sending...'}
 
             </div>
           </button>
