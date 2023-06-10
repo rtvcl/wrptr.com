143159d22464a926306289aa4805176f6ca77874
rtcl <rtvcl.boss@gmail.com>
2023-06-10 18:57:03 +0700

    jangan ditiru di rumah

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,9 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-
-    console.log('test')
-
   let nodemailer = require("nodemailer");
   const transporter = nodemailer.createTransport({
     port: 465,
@@ -18,19 +15,23 @@ export async function POST(req: NextRequest) {
   const bodyRequest = await req.json();
 
   const mailData = {
-    from: 'local@local.com',
-    to: 'khainciller@gmail.com',
+    from: "local@local.com",
+    to: "khainciller@gmail.com",
     subject: `Message From ${bodyRequest.name}`,
     text: bodyRequest.message,
-    html: `<div>${bodyRequest.message}</div>`
-   }
+    html: `<div>${bodyRequest.message}</div>`,
+  };
 
-   transporter.sendMail(mailData, function (err: any, info: any) {
-    if(err)
-      console.log(err)
-    else
-      console.log(info)
-  })
+  let test;
+  transporter.sendMail(mailData, function (err: any, info: any) {
+    if (err) {
+      console.log(err);
+      test = info;
+    } else {
+      console.log(info);
+      test = info;
+    }
+  });
 
-  return NextResponse.json({ message: "success send message" });
+  return NextResponse.json({ message: "success send message",test,mailData });
 }
