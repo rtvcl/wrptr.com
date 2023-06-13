da0a8180451744d4e9a94b1ea1cb7fbdc93db3bc
rtcl <rtvcl.boss@gmail.com>
2023-06-13 20:56:42 +0700

    fix: trying usestate on scroll 1

--- src/hooks/use-scroll.tsx
+++ src/hooks/use-scroll.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 
 export const useScroll = () => {
-  const [scrollY, setScrollY] = useState(1);
+  const [scrollY, setScrollY] = useState(0);
   const onScroll = useCallback(() => {
     const { pageYOffset, scrollY } = window;
     setScrollY(window.pageYOffset);
