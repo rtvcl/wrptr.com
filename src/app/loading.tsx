35dc6ab76b15d9533468fe65a4b68bd09d943236
rtcl <rtvcl.boss@gmail.com>
2023-06-21 19:19:55 +0700

    feat: add green loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <div className="flex items-center justify-center min-h-[80vh]">
+    <span className="flex items-center justify-center h-20 loader"></span>
+  </div>
+}
