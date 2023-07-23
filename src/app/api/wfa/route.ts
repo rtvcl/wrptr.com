8204d17b9abab1551679ce0ca7068cf39144e975
rtcl <rtvcl.boss@gmail.com>
2023-07-23 15:39:08 +0700

    feat: add pagination functionality

--- src/app/api/wfa/route.ts
+++ src/app/api/wfa/route.ts
@@ -3,17 +3,18 @@ import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
 
-    // GET /items?limit=20&offset=20 ✅
-    // GET /items?page=1
-
+    const search = req.nextUrl.searchParams.get('q')
+    const page = parseInt(req.nextUrl.searchParams.get('page') || '1')
     const limit = parseInt(req.nextUrl.searchParams.get('limit') || '3')
     const offset = parseInt(req.nextUrl.searchParams.get('offset') || '0')
-    const page = parseInt(req.nextUrl.searchParams.get('page') || '1')
-    const search = req.nextUrl.searchParams.get('q')
+
+    const skipTake = {
+        skip: ((page - 1) * limit) + offset,
+        take: limit
+    }
 
     const places = await prisma.place.findMany({
-        skip: offset,
-        take: limit,
+        ...(skipTake),
         ...(search && {
             where: {
                 OR: [
@@ -31,5 +32,25 @@ export async function GET(req: NextRequest) {
             }
         })
     })
-    return NextResponse.json({ data: places })
+
+    const placesCount = await prisma.place.count({
+        ...(search && {
+            where: {
+                OR: [
+                    {
+                        name: {
+                            contains: search
+                        }
+                    },
+                    {
+                        location_name: {
+                            contains: search
+                        }
+                    }
+                ]
+            }
+        })
+    })
+
+    return NextResponse.json({ data: places, meta: { total: placesCount, total_page: Math.ceil(placesCount / limit) } })
 }
