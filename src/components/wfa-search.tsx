584cd50d220eee108ae72b113379eac38d8db82c
rtcl <rtvcl.boss@gmail.com>
2023-09-08 15:09:22 +0700

    add search button

--- src/components/wfa-search.tsx
+++ src/components/wfa-search.tsx
@@ -23,7 +23,7 @@ const WfaSearch = ({ initialSearch }: Props) => {
         router.replace(`/wfa?search=${encodeURIComponent(search)}`);
         router.refresh();
       }}
-      className="flex items-center w-full gap-2 mt-4">
+      className="flex w-full gap-2 mt-4">
       <input
         className="border-2 flex-1 border-box bg-[#f1f1f1] border-[#000000] px-4 py-2 text-sm"
         type="text"
@@ -31,7 +31,7 @@ const WfaSearch = ({ initialSearch }: Props) => {
         value={search}
         onChange={(e) => setSearch(e.currentTarget.value)}
       />
-      <button type="submit" className="hidden" />
+      <button type="submit" className="px-4 text-base font-bold border-[#000000] border-[1px] retro-shadow-sm hover:text-white hover:bg-black transition-colors flex gap-2 items-center ">Search</button>
     </form>
   );
 };
