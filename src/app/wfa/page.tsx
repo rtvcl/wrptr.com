7187fcd5770ecee5f86e4884eb7952614530cc17
rtcl <rtvcl.boss@gmail.com>
2023-12-19 16:14:55 +0700

    refactor search page

--- src/app/wfa/page.tsx
+++ src/app/wfa/page.tsx
@@ -9,26 +9,42 @@ type Props = {
   };
 };
 
+type WfaPlacesResponse = {
+  data: Place[];
+  meta: { total: number; page: number; total_page: number };
+};
+
+const buildQueryString = (search?: string) => {
+  if (!search) return "";
+  return `?q=${encodeURIComponent(search)}`;
+};
+
+const fetchPlacesByWfaSearch = async (options: { search?: string; page?: number } = {}) => {
+  try {
+    const response = await fetch(
+      `${process.env.BASE_URL}/api/wfa${buildQueryString(options.search)}`,
+      { method: "GET" }
+    );
+    const data: WfaPlacesResponse = await response.json();
+    return data;
+  } catch (error) {
+    // handle error based on your needs
+    console.error(error);
+  }
+};
+
+
 const WFCList = async ({ searchParams: { search } }: Props) => {
-  const response = await fetch(
-    `${process.env.BASE_URL}/api/wfa${search ? `?q=${encodeURIComponent(search)}` : ""
-    }`,
-    { method: "GET" }
-  );
-  const {
-    data,
-    meta,
-  }: {
-    data: Place[];
-    meta: { total: number; page: number; total_page: number };
-  } = await response.json();
+  const wfaResponse = await fetchPlacesByWfaSearch({ search })
+  
+  if(!wfaResponse) return
 
   return (
     <section className="max-w-screen-lg mx-4 mb-12 lg:mx-auto">
       <WFAIntroduction />
       <div>
         <WfaSearch initialSearch={search} />
-        <WFAList places={data} total_page={meta.total_page} />
+        <WFAList places={wfaResponse.data} total_page={wfaResponse.meta.total_page} />
       </div>
     </section>
   );
